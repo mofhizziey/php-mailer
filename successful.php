5e931c8d18012937eb6e0319db6e32f4554f7a63
Enimofe <47665473+mofhizziey@users.noreply.github.com>
2020-12-07 22:42:20 +0100

    Create successful.php

---
+++ successful.php
@@ -0,0 +1,22 @@
+<?php 
+include("server.php")
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<link rel="stylesheet" href="./bootstrap/dist/css/bootstrap.min.css">
+<body><br><br>
+<div class='container text-center p-5'>
+    <p style='font-size:35px; font-weight:bold;' class='lead'>Congratulations!, your mail  is on its way (*Sniffed*)</p><br>
+    <a href='mailer.php' class='btn btn-danger btn-block'>Go back to mailer</a>
+    </div>
+    
+    
+    
+
+</body>
+</html>
